d63b7a5b00ef45bc1d54ae9ebdd89e59497d52b0
Daniel Gillespie <daniel.gillespie0949@gmail.com>
2020-03-15 10:50:42 -0700

    working on ui

--- src/main/java/app/views/FrameMain.java
+++ src/main/java/app/views/FrameMain.java
@@ -1,10 +1,16 @@
 package app.views;
 
 import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreeNode;
 import java.awt.*;
+import java.awt.event.KeyEvent;
 
 public class FrameMain {
     private JFrame frame;
+    private JMenuBar menu;
+    private JTree treeContactList;
+    private JCheckBox chkAllowDiscovery;
 
     public void show() throws Exception {
         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
@@ -17,8 +23,20 @@ public class FrameMain {
 
         frame.setLayout(new BorderLayout());
 
-        var tree = new JTree();
-        frame.add(tree, BorderLayout.CENTER);
+        menu = new JMenuBar();
+        var menuFile = new JMenu("File");
+        menuFile.setMnemonic(KeyEvent.VK_F);
+        menuFile.add(new JMenuItem("Exit"));
+        menu.add(menuFile);
+        frame.add(menu, BorderLayout.NORTH);
+
+        var rootNode = new DefaultMutableTreeNode("Contacts");
+        treeContactList = new JTree(rootNode);
+        treeContactList.setRootVisible(false);
+        frame.add(treeContactList, BorderLayout.CENTER);
+
+        chkAllowDiscovery = new JCheckBox("Allow Discovery");
+        frame.add(chkAllowDiscovery, BorderLayout.SOUTH);
 
         frame.setVisible(true);
     }
